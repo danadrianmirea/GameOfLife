9647c857cd74a6f36b6c7a1ae1d247a5e7e3db93
Dan Adrian Mirea <damirea84@gmail.com>
2024-06-28 01:24:51 +0300

    remove init.sh script

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# GameOfLife" >> README.md
-git init
-git add .
-git commit -m "Game Of life first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/GameOfLife.git
-git push -u origin main
