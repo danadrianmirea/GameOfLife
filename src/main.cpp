6e93808510e1e4fb960e4ab2594a71f751b649b5
Dan Adrian Mirea <damirea84@gmail.com>
2025-04-19 03:46:14 +0300

    add emscripten build

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
-
 #include "raylib.h"
 #include "globals.h"
 #include "game.h"
 #include <iostream>
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+Game* game = nullptr;
+
+void mainLoop()
+{
+    float dt = GetFrameTime();
+    game->Update(dt);
+    game->Draw();
+}
 
 int main()
 {
@@ -10,18 +21,20 @@ int main()
     SetExitKey(KEY_NULL);
     SetTargetFPS(144);
     
-    Game game(gameScreenWidth, gameScreenHeight, offset, gameScreenWidth / (numCells + 2));
-    game.Randomize();
+    game = new Game(gameScreenWidth, gameScreenHeight, offset, gameScreenWidth / (numCells + 2));
+    game->Randomize();
     ToggleBorderlessWindowed();
-    float dt = 0.0f;
 
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(mainLoop, 0, 1);
+#else
     while (!exitWindow)
     {
-        dt = GetFrameTime();
-        game.Update(dt);
-        game.Draw();
+        mainLoop();
     }
-
+    delete game;
     CloseWindow();
+#endif
+
     return 0;
 }
