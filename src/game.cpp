5e9fad351250d875b278ce9feef0d1f8da3e888a
Dan Adrian Mirea <damirea84@gmail.com>
2025-04-23 08:50:48 +0300

    fix ui elements in web mobile

--- src/game.cpp
+++ src/game.cpp
@@ -248,7 +248,7 @@ void Game::DrawUI()
     // DrawRectangleRoundedLines({borderOffsetWidth, borderOffsetHeight, gameScreenWidth - borderOffsetWidth * 2, gameScreenHeight - borderOffsetHeight * 2}, 0.18f, 20, 2, yellow);
     DrawTextEx(font, "Conway's game of life", {200, 10}, 34, 2, yellow);
     if (isMobile) {
-        DrawTextEx(font, "Tap screen to start/reset", {50, 50}, 34, 2, yellow);
+        DrawTextEx(font, "Tap screen to start/reset", {160, 50}, 34, 2, yellow);
     } else {
         DrawTextEx(font, "ESC to exit, P to pause, Enter to restart", {50, 50}, 34, 2, yellow);
     }
@@ -262,7 +262,7 @@ void Game::DrawUI()
     {
         DrawRectangleRounded({screenX + (float)(gameScreenWidth / 2 - 250), screenY + (float)(gameScreenHeight / 2 - 20), 500, 60}, 0.76f, 20, BLACK);
         if (isMobile) {
-            DrawText("Tap to play", screenX + (gameScreenWidth / 2 - 100), screenY + gameScreenHeight / 2, 20, yellow);
+            DrawText("Tap to play", screenX + (gameScreenWidth / 2 - 60), screenY + gameScreenHeight / 2, 20, yellow);
         } else {
             DrawText("Press Enter to play", screenX + (gameScreenWidth / 2 - 100), screenY + gameScreenHeight / 2, 20, yellow);
         }
